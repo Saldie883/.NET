482d1b4aa3c792590ff4ee256f9e9790fc867ff1
Sasha Gorbar <sanyagor23@gmail.com>
2020-06-17 14:35:41 +0300

    Create CarType.cs

---
+++ Task3/Models/CarType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3.Models
+{
+    internal class CarType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
